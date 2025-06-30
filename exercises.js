1ad35edd7b5c493acc2275c6168ed5bad72682fd
ha0177 <214249506+Ha0177@users.noreply.github.com>
2025-06-30 15:45:16 +0300

    Add groupById function to exercises.js for creating keyed objects from user arrays

--- exercises.js
+++ exercises.js
@@ -136,3 +136,26 @@ function getAverageAge(users) {
     }, 0);
     return sum / users.length;
 };
+
+
+// Create keyed object from array
+
+// Let’s say we received an array of users in the form {id:..., name:..., age:... }.
+
+// Create a function groupById(arr) that creates an object from it, with id as the key, and array items as values.
+
+let users = [
+    {id: 'john', name: "John Smith", age: 20},
+    {id: 'ann', name: "Ann Smith", age: 24},
+    {id: 'pete', name: "Pete Peterson", age: 31},
+  ];
+
+  function groupById(users) {
+    return users.reduce((acc, user) => {
+    acc[user.id] = user;
+    return acc;        
+    }, {});
+    
+  }
+
+  console.log(groupById(users));
